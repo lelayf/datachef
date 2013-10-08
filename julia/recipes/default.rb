e6f70a90e45f8acf8ba0b9d82927ae6caf58a1ad
François Le Lay <mfworx@gmail.com>
2013-10-08 01:44:35 +0000

    add julia cookbook

---
+++ julia/recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: julia
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+apt_repository "julia-ppa" do
+  uri "http://ppa.launchpad.net/staticfloat/julianightlies/ubuntu"
+  distribution "precise"
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "C0061A4A"
+  action :add
+  notifies :run, "execute[apt-get update]", :immediately
+end
+
+apt_repository "julia-deps-ppa" do
+  uri "http://ppa.launchpad.net/staticfloat/julia-deps/ubuntu"
+  distribution "precise"
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "C0061A4A"
+  action :add
+  notifies :run, "execute[apt-get update]", :immediately
+end
+
+package "julia" do
+  action :install
+end
+
